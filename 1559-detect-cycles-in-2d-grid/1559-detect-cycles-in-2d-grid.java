8853d218e669b6b868375820412b86813761b598
Sadab Ehtesham <csds23083@glbitm.ac.in>
2026-04-26 12:37:21 +0530

    Time: 21 ms (39.56%), Space: 145.8 MB (16.8%) - LeetHub

---
+++ 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.java
@@ -0,0 +1,48 @@
+class Solution {
+    public boolean containsCycle(char[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        boolean[][] visited = new boolean[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!visited[i][j]) {
+                    // Start DFS for each unvisited cell
+                    if (dfs(grid, visited, i, j, -1, -1, grid[i][j])) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean dfs(char[][] grid, boolean[][] visited, int r, int c, int pr, int pc, char target) {
+        visited[r][c] = true;
+
+        int[] dr = {0, 0, 1, -1};
+        int[] dc = {1, -1, 0, 0};
+
+        for (int i = 0; i < 4; i++) {
+            int nr = r + dr[i];
+            int nc = c + dc[i];
+
+            // Check boundaries and if the character matches the target
+            if (nr >= 0 && nr < grid.length && nc >= 0 && nc < grid[0].length && grid[nr][nc] == target) {
+                // If the neighbor is already visited and is NOT the parent, a cycle is found
+                if (visited[nr][nc]) {
+                    if (nr != pr || nc != pc) {
+                        return true;
+                    }
+                    continue;
+                }
+
+                // Recursive DFS call
+                if (dfs(grid, visited, nr, nc, r, c, target)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
